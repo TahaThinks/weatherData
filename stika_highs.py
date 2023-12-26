e478ab66022879340e1c26d15cfae6dd8acc0396
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-26 18:18:17 +0400

    Extracting Low Temperatures from csv

--- stika_highs.py
+++ stika_highs.py
@@ -8,11 +8,14 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get dates and high temperatures from this file.
-    dates, highs = [], []
+    # Get dates, high and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         high = int(row[5])
         highs.append(high)
+
+        low = int(row[6])
+        lows.append(low)
         
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
         dates.append(current_date)
@@ -20,7 +23,7 @@ with open(filename) as f:
 # Plot the high temperatures.
 plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red') #X-axis is Dates & y-axis is highs
+ax.plot(dates, highs, c='red') #X-axis is Dates & y-axis is high temperature
 
 # Format Plot.
 plt.title("Daily high Temperatures - 2018", fontsize=24)
