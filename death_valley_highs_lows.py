d18ad82f750de5428994c0199354fb5cb67aee98
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-26 18:47:17 +0400

    Extract Date, low and high temperatures from Death_Valley CSV

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,2 +1,22 @@
 import csv
+from datetime import datetime
 import matplotlib.pyplot as plt
+
+# 0 STATION  1 NAME 2 DATE  3 PRCP  4 TMAX 5 TMIN  6 TOBS
+
+filename = 'data/death_valley_2018_simple.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    highs, lows, dates = [],[],[]
+    for row in reader:
+        high = int(row[4])
+        highs.append(high)
+
+        low = int(row[5])
+        lows.append(lows)
+
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(current_date)
