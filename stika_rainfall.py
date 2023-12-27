ead29b10cb8f9b947b58e231de44e98b7e0e7018
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-27 17:55:25 +0400

    Extracting related data from csv

--- stika_rainfall.py
+++ stika_rainfall.py
@@ -1,3 +1,10 @@
 import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
+
+filename = 'data/skita_weather_2018_simple.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    
