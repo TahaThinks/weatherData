f0f3fcc6291f6e5c334c15b0b051bb5eb44a432e
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-27 18:33:27 +0400

    Add Death Valley Data to the Plot

--- stika_rainfall.py
+++ stika_rainfall.py
@@ -12,38 +12,38 @@ with open(filename1) as f:
 
     skita_rainfall, skita_dates = [],[]
     for row in reader:
-        daily_rain = float(row[3])
-        skita_rainfall.append(daily_rain)
+        stika_daily_rain = float(row[3])
+        skita_rainfall.append(stika_daily_rain)
 
-        current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        skita_dates.append(current_date)
+        stika_current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        skita_dates.append(stika_current_date)
 
 # Extracting Rainfall & Dates for DeathValley
 with open(filename2) as f:
-    reader = csv.reader(filename2)
+    reader = csv.reader(f)
     header_row = next(reader)
 
     dv_rainfall, dv_dates = [],[]
     for row in reader:
-        current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        dv_dates.append(current_date)
         try:
-            daily_rain = float(row[3])
+            dv_current_date = datetime.strptime(row[2], "%Y-%m-%d")
+            dv_daily_rain = float(row[3])
         except:
-            print(f"Data not available for Date: {current_date}")
+            print(f"Data not available for Date:{dv_current_date}")
         else:
-            dv_rainfall.append(daily_rain)
-            dv_dates.append(current_date)
+            dv_rainfall.append(dv_daily_rain)
+            dv_dates.append(dv_current_date)
 
 
 # Plot the Rainfall with respect to the Date
 plt.style.use('seaborn-v0_8-bright')
 fig, ax = plt.subplots()
 ax.plot(skita_dates, skita_rainfall, c="dodgerblue")
+ax.plot(dv_dates, dv_rainfall, c="darkviolet")
 
 
 # Format Plot:
-plt.title("Daily Rainfall in Stika - 2018", fontsize=24)
+plt.title("Daily Rainfall in Stika vs Death Valley - 2018", fontsize=24)
 plt.xlabel(" ", fontsize=14)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=14)
