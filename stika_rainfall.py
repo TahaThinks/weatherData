e0450fc7642bf3547ed266e349fd3d6f12b4551f
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-27 18:18:52 +0400

    Extracting Data for DV CSV

--- stika_rainfall.py
+++ stika_rainfall.py
@@ -2,26 +2,44 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename1 = 'data/sitka_weather_2018_simple.csv'
+filename2 = 'data/death_valley_2018_simple.csv'
 
-# Extracting Rainfall & Dates:
-
-with open(filename) as f:
+# Extracting Rainfall & Dates for Skita:
+with open(filename1) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    rainfall, dates = [],[]
+    skita_rainfall, skita_dates = [],[]
     for row in reader:
         daily_rain = float(row[3])
-        rainfall.append(daily_rain)
+        skita_rainfall.append(daily_rain)
 
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        dates.append(current_date)
+        skita_dates.append(current_date)
+
+# Extracting Rainfall & Dates for DeathValley
+with open(filename2) as f:
+    reader = csv.reader(filename2)
+    header_row = next(reader)
+
+    dv_rainfall, dv_dates = [],[]
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dv_dates.append(current_date)
+        try:
+            daily_rain = float(row[3])
+        except:
+            print(f"Data not available for Date: {current_date}")
+        else:
+            dv_rainfall.append(daily_rain)
+            dv_dates.append(current_date)
+
 
 # Plot the Rainfall with respect to the Date
 plt.style.use('seaborn-v0_8-bright')
 fig, ax = plt.subplots()
-ax.plot(dates, rainfall, c="dodgerblue")
+ax.plot(skita_dates, skita_rainfall, c="dodgerblue")
 
 
 # Format Plot:
